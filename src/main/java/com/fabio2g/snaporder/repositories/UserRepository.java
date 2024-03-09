d2f77b94ada8fc62ced26c1e3056904c1302d4e4
fabio2g <fabiodev88@gmail.com>
2024-03-09 17:48:43 -0300

    adicionado método de listar todos usuários e listar por ID

--- src/main/java/com/fabio2g/snaporder/repositories/UserRepository.java
+++ src/main/java/com/fabio2g/snaporder/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.fabio2g.snaporder.repositories;
 import com.fabio2g.snaporder.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
