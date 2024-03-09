4de0c38b69bfb69f30912c0c8ccb282ff6536878
fabio2g <fabiodev88@gmail.com>
2024-03-09 16:27:19 -0300

    configurando o projeto

---
+++ src/main/java/com/fabio2g/snaporder/SnapOrderApplication.java
@@ -0,0 +1,13 @@
+package com.fabio2g.snaporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio2g/snaporder/SnapOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio2g.snaporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
